b27b0226737acaba75f884f31e3846370f1f5b6f
Mohsen Adeli <mohsen.adelie@gmail.com>
2021-10-25 22:49:12 +0330

    add spring bean xml configuration.

--- src/main/java/com/adeli/joke/jokeapp/JokeAppApplication.java
+++ src/main/java/com/adeli/joke/jokeapp/JokeAppApplication.java
@@ -2,12 +2,14 @@ package com.adeli.joke.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokeAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JokeAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JokeAppApplication.class, args);
+    }
 
 }
